4fbf55324758f048800f98d53e901e15e78204e0
gopal <gopalpeddi379@gmail.com>
2023-06-02 23:32:33 +0530

    implemented passport strategy

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
 const express = require("express");
 const app = express();
-var csrf = require("csurf");
+var csrf = require("tiny-csrf");
 var cookieParser = require("cookie-parser");
 const path = require("path");
 const { User } = require("./models");
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 app.use(express.urlencoded({ extended: false }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser("Shh! Some Secret String"));
-app.use(csrf({ cookie: true }));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELTE"]));
 
+app.use(session({
+  secret: "my-super-secret-key-24386981602896017092640982168",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passportField: 'password'
+}, (username, password, done) => {
+  User.findOne({ where: { email: username, password: password } })
+    .then((user) => {
+    return done(null, user);
+    }).catch((err) => {
+      return (err);
+  })
+}));
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err, null);
+    })
+});
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -35,6 +78,7 @@ app.get("/signup", (req, res) => {
 });
 
 app.post("/users", async (req, res) => {
+  const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
   const subval = req.body.submit;
   try {
     if (subval === "admin") {
@@ -42,21 +86,34 @@ app.post("/users", async (req, res) => {
         firstname: req.body.firstName,
         lastname: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPwd,
         role: "admin",
+      });
+      req.login(admin, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/sports");
       });
     } else if (subval === "player") {
       const player = await User.create({
         firstname: req.body.firstName,
         lastname: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPwd,
         role: "player",
+      });
+      req.login(player, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/sports");
       });
     }
   } catch (err) {
     console.log(err);
+    return response.redirect("/signup");
   }
-})
+});
 
 module.exports = app;
