b9382b500f4d91f416b953db3f4e1094d894edca
gopal <gopalpeddi379@gmail.com>
2023-06-14 18:15:55 +0530

    added tests for signup as admin or player,signout, and precommit

---
+++ __tests__/sport.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const request = require("supertest");
+var cheerio = require("cheerio");
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
+const login = async (agent, username, password) => {
+  let res = await agent.get("/login");
+  let csrfToken = extractCsrfToken(res);
+  res = await agent.post("/session").send({
+    email: username,
+    password: password,
+    _csrf: csrfToken,
+  });
+};
+
+describe("Sports Scheduler", function () {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(4000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  test("Sign Up as Admin", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/users").send({
+      firstName: "John",
+      lastName: "Smith",
+      email: "john@smith.com",
+      password: "123456789",
+      submit: "admin",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("Sign Up as Player", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/users").send({
+      firstName: "James",
+      lastName: "Doe",
+      email: "james@doe.com",
+      password: "123456789",
+      submit: "player",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("Sign Out", async () => {
+    let res = await agent.get("/sport");
+    expect(res.statusCode).toBe(200);
+    res = await agent.get("/signout");
+    expect(res.statusCode).toBe(302);
+    res = await agent.get("/sport");
+    expect(res.statusCode).toBe(302);
+  });
+});
